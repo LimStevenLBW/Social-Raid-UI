946e71328823cdb945688d3786f290f6225017c0
Steven Lim <limstevenlbw@gmail.com>
2019-01-18 01:26:18 -0800

    configlog changes

--- src/ConfigContainer/Changelog.jsx
+++ src/ConfigContainer/Changelog.jsx
@@ -5,15 +5,20 @@ function Changelog(props) {
         return (
             <div id="changelog">
                 <span className="notification">
-                    <p id="version-name">Social Raid version 1.1</p>
+                    <p id="version-name">Social Raid version 1.1.0</p>
                     <ul>
                         <li>Added a customizable description of rewards for streamers to use</li>
-                        <li>Adjusted the visibility of some interface elements</li>
-                        <li>Some miscellaneous bug fixes</li>
+                        <li>Adjusted the visibility of interface elements</li>
+                        <li>Miscellaneous bug fixes</li>
                     </ul>
 
-                    <div>  <a className="support-link" href='mailto:support@moopaloo.com'>Need Support?</a>
-                        <a className="support-link" target="_blank" href='https://twitter.com/social_raid'>Our Twitter</a></div>
+
+                    <div id="support-description">
+                        <p >Questions, comments, or concerns? Contact us!</p>
+                        <a className="support-link" href='mailto:support@moopaloo.com'>Our Email</a>
+                        <a className="support-link" target="_blank" href='https://twitter.com/social_raid'>Our Twitter</a>
+                    </div>
+
 
                 </span>
 
